ab3c70ff34bc1581448cc3f508d51b2931d219dd
Bromantic <salisburydonte@gmail.com>
2016-08-12 19:39:35 -0700

    no message

--- demo.js
+++ demo.js
@@ -1,97 +1,108 @@
 //var jQuery = require('jquery-deferred'); // npm install jquery-deferred
 var fs = require("fs"); //filewriting/reading library
-var dbcards = require("./cards.json")
+var dbcards = require("./cards.json");
+    //var redis = require("redis") // npm install redis
+    //client = redis.createClient();
+
 //var request = require('request'); //https://www.npmjs.com/package/request
 //var remotefile = 'https://raw.githubusercontent.com/Bromantic/YGOPro-Web/master/cards.json'
-var dbdesc = require("./desc/cardlibs.js") // Card Library
-var functions = require("./libs/userfunctions.js") // Custom Functions
+var dbdesc = require("./desc/cardlibs.js"); // Card Library
+var functions = require("./libs/userfunctions.js"); // Custom Functions
 var carddata = {
-	type: "",
-	attribute: "",
-	race: "",
-	level: "",
-	lscale: "",
-	rscale: "",
-	desc: "",
-	atk: "",
-	def: "",
-	name: "",
-	id: "",
-	setcode: ""
-	}
-/* request(remotefile, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-	var webdb = JSON.parse(body) */
-	//console.log(webdb[1].name)
-	// var dbinfo = webdb[6999]
-	var dbinfo = dbcards[6999] // Sample Card Value
-	var check = dbinfo.type
-	carddata.level = functions.parselevel(dbinfo.level).level
-	carddata.lscale = functions.parselevel(dbinfo.level).lscale
-	carddata.rscale = functions.parselevel(dbinfo.level).rscale
-	TextParse(dbinfo)
-	//console.log(dbinfo.name)
-	//console.log(dbdata)
-	console.log(carddata)
-/*   }
-}) */
+        type: "",
+        attribute: "",
+        race: "",
+        level: "",
+        lscale: "",
+        rscale: "",
+        desc: "",
+        atk: "",
+        def: "",
+        name: "",
+        id: "",
+        setcode: ""
+    };
+    /* request(remotefile, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+      var webdb = JSON.parse(body) */
+    //console.log(webdb[1].name)
+    // var dbinfo = webdb[6999]
+var dbinfo = dbcards[2]; // Sample Card Value
+    //var dbinfo = dbcards[6999] // Sample Card Value
+TextUpdate(dbinfo);
+var check = dbinfo.type;
+carddata.level = functions.parselevel(dbinfo.level).level;
+carddata.lscale = functions.parselevel(dbinfo.level).lscale;
+carddata.rscale = functions.parselevel(dbinfo.level).rscale;
+TextParse(dbinfo);
+    //console.log(dbdata)
+console.log(carddata);
+    /*   }
+    }) */
 
-function TextConvert(amount,hex) { // Greedy Algorithm to divide Hex groups
-		var diff = []
-		var total = 0;
-		var tempkey = functions.reverse(Object.keys(hex))
-		tempkey.forEach(function(nums) {
-		nums = parseInt(nums)
-	
-	//nums = Math.floor(nums)
-    while (total + nums <= amount) {
-	  diff.push(nums);
-      total += nums;
-	  //console.log(diff)
-    }
-  });
-  console.log(diff)
-  return diff;
-};
+function TextConvert(amount, hex) { // Greedy Algorithm to divide Hex groups
+    "use strict";
+    var diff = [],
+        total = 0,
+        tempkey = functions.reverse(Object.keys(hex));
+    tempkey.forEach(function (nums) {
+        nums = parseInt(nums, 10); 
+        //nums = Math.floor(nums)
+        while (total + nums <= amount) {
+            if (total + nums <= 0) {
+            break;
+            }
+            diff.push(nums);
+            total += nums;
+            //console.log(diff)
+            //console.log(total+"+"+nums+"<="+amount)
+            //console.log(total)
+            
+        }
+    });
+    //console.log(diff)
+    return diff;
+}
 
 function TextParse(info) {
-Object.keys(dbdesc).forEach(function(num) { // Loop through Type, Attribute, and Race to readable text
-		var check = info[num]
-		var dbdata = []
-		text = dbdesc[num]
-		var hexkeys = text
-		var ammt = TextConvert(check,hexkeys);
-		//console.log(ammt)
-		var HexText = []
-		var arrayLength = ammt.length;
-		for (var i = 0; i < arrayLength; i++) {
-		//console.log(ammt[i])
-		
-		dbdata.push(hexkeys[ammt[i]])
-		//console.log(hexsearch(ammt[i]))
-		}
-		carddata[num] = dbdata.join('/');
-		if (dbdata.join('/') === "") {
-		carddata[num] = "None"
-		}
-		//console.log(carddata[num])
-		
-});
+    "use strict";
+    Object.keys(dbdesc).forEach(function (num) { // Loop through Type, Attribute, and Race to readable text
+        //console.log(num)
+        var check = info[num],
+            dbdata = [],
+            text = dbdesc[num],
+            hexkeys = text,
+            ammt = TextConvert(check, hexkeys),
+            HexText = [],
+            arrayLength = ammt.length;
+        for (var i = 0; i < arrayLength; i++) {
+            //console.log(ammt[i])
+
+            dbdata.push(hexkeys[ammt[i]]);
+        }
+        carddata[num] = dbdata.join('/');
+        if (dbdata.join('/') === "") {
+            carddata[num] = "None";
+        };
+        //console.log(carddata[num])
+        console.log(carddata.setcode)
+
+    });
 };
 
-/* function TextUpdate(info) { // This is 2nd
-carddata = {
-	type: "",
-	attribute: "",
-	race: "",
-	level: "",
-	lscale: "",
-	rscale: "",
-	desc: "",
-	atk: "",
-	def: "",
-	name: "",
-	id: "",
-	setcode: ""
-	}
-}; */
+function TextUpdate(info) { // This is 2nd
+    carddata = {
+        type: "",
+        attribute: "",
+        race: "",
+        level: "",
+        lscale: "",
+        rscale: "",
+        desc: info.desc,
+        atk: info.atk,
+        def: info.def,
+        name: info.name,
+        id: info.id,
+        setcode: ""
+    }
+};
--- libs/userfunctions.js
+++ libs/userfunctions.js
@@ -1,18 +1,34 @@
 exports.reverse = function reverse(a) {
-    var temp = a
-    var i = 0;
-    var j = a.length - 1;
+    "use strict";
+    var temp = a,
+        i = 0,
+        j = a.length - 1,
+        x;
+    
     while (i < j) {
-        var x = a[i];
+        x = a[i];
         a[i] = a[j];
         a[j] = x;
         i++;
         j--;
     }
     return a;
-}
+};
 
-exports.parselevel = function parselevel(rawlevel) {
+exports.parselevel = function parselevel(lvl) {
+    "use strict";
+    var cd = {
+            level: 0,
+            lscale: 0,
+            rscale: 0
+	    };
+    cd.level = lvl & 0xff;
+    cd.lscale = (lvl >> 24) & 0xff;
+    cd.rscale = (lvl >> 16) & 0xff;
+    return cd;
+};
+
+/* exports.parselevel = function parselevel(rawlevel) {
 var cardinfo = {
 lscale: "",
 rscale: "",
@@ -40,4 +56,5 @@ cardinfo.level = parseInt(hex.substring(6, 7), 16)
 //alert(hex.substring(0, 1)+" and "+hex.substring(2, 3)+" and "+hex.substring(6, 7))
 //alert(cardinfo.lscale+" and "+cardinfo.rscale)
 return cardinfo;
-};
+}; */
+
