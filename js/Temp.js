29fda6f68251c1834c3113adfd5612d67cb56975
Bromantic <salisburydonte@gmail.com>
2016-08-29 18:42:52 -0700

    no message

--- js/Temp.js
+++ js/Temp.js
@@ -33,6 +33,14 @@ var carddata = {
         format: "",
         setcode: ""
     }; 
+
+/*exphbs.registerHelper('ifCond', function(v1, options) {
+  if(v1 !== undefined || v1 !== "") {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});*/
+
 var isFound;
 app.engine('handlebars', 
     exphbs({defaultLayout: 'main'})); 
